6a16570244a1151b2db3e01db39a5a1e74c22d4c
Erica L. Ingram <inquiries@aquoco.co>
2020-01-17 06:49:36 -0800

    added rocket + animation, header/footer pictures/text, imported 2 fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+	constructor() {
+		super();
 		this.state = {
 			searchTerm: ''
 		};
@@ -15,15 +18,11 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
+				<Header />
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-						Learn React
-					</a>
+					<p>search containers will go here.</p>
 				</header>
+				<Footer />
 			</div>
 		);
 	}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,70 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import spaceBanner from './../img/space-banner.jpg';
+import rocket from './../img/rocket.svg';
+
+const ImgHeader = styled.header`
+	background-image: url(${spaceBanner});
+	width: 100%;
+	color: #ffffff;
+	overflow: hidden;
+`;
+
+const wobble3 = keyframes`
+	0% {
+        transform: rotate(180deg) translateX(90vh) translateY(0%);
+	}
+    25% {
+        transform: rotate(180deg) translateX(50vh) translateY(-20%);
+    }
+    50% {
+        transform: rotate(180deg) translateX(-70vh) translateY(0%);
+    }
+    75% {
+        transform: rotate(180deg) translateX(-110vh) translateY(-10%);
+    }
+    100% {
+        transform: rotate(180deg) translateX(-150vh) translateY(-20%);
+    }
+`;
+const Rocket = styled.img`
+	src: url(${rocket});
+	justify-content: right;
+	align-self: right;
+	width: 10%;
+	padding-top: 2%;
+	color: #ffffff;
+	transform: translate(-2vh, -6vh);
+	animation: ${wobble3};
+	animation-duration: 7s;
+	animation-iteration-count: infinite;
+	animation-timing-function: linear;
+	z-index: 99000;
+	overflow: hidden;
+`;
 
 // need rocket animation sliding in and out of screen side to side
 
-function Header() {
-	return (
-		<div>
-			<header>
-				<h2>THIS IS THE NASA SEARCH HEADER!</h2>
-			</header>
-		</div>
-	);
+class Header extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			searchTerm: ''
+		};
+	}
+	render() {
+		return (
+			<div>
+				<ImgHeader>
+					<div id="rocket" class="rocket">
+						<Rocket src={rocket} />
+					</div>
+					<h2>Welcome to Erica Ingram's</h2>
+					<h2>NASA Multimedia Library Search Clone</h2>
+				</ImgHeader>
+			</div>
+		);
+	}
 }
 
 export default Header;
