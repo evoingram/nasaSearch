602b4c1f9dedadc38cfd63e30112467b9efefed2
Erica L. Ingram <inquiries@aquoco.co>
2020-01-18 09:24:54 -0800

    added nasa links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,10 +27,27 @@ function Footer() {
 		<div>
 			<ImgHeader>
 				<h2>
-					{' '}
-					<Link href="https://www.ericaingram.com/" target="_blank">
-						See more of Erica's work in her portfolio.
-					</Link>
+					<div>
+						{' '}
+						<Link href="https://www.ericaingram.com/" target="_blank">
+							See more of Erica's work in her portfolio.
+						</Link>
+					</div>
+					<div>
+						{' '}
+						<Link
+							href="http://www.nasa.gov/audience/formedia/features/MP_Photo_Guidelines.html"
+							target="_blank"
+						>
+							NASA's Usage Guidelines
+						</Link>
+					</div>
+					<div>
+						{' '}
+						<Link href="https://www.nasa.gov/about/contact/index.html" target="_blank">
+							Contact NASA
+						</Link>
+					</div>
 				</h2>
 			</ImgHeader>
 		</div>
