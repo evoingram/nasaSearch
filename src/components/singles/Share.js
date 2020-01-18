ef1dc840f3c639f4df5d8da0f4b31d2ff4e909aa
Erica L. Ingram <inquiries@aquoco.co>
2020-01-18 09:12:12 -0800

    completed share component

--- src/components/singles/Share.js
+++ src/components/singles/Share.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import {
+	EmailShareButton,
+	FacebookShareButton,
+	LinkedinShareButton,
+	PinterestShareButton,
+	PocketShareButton,
+	RedditShareButton,
+	TumblrShareButton,
+	TwitterShareButton,
+	WhatsappShareButton
+} from 'react-share';
 
-function Header() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Share extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Share this media:</p>
+					<EmailShareButton url={this.props.fileURL} />
+					<FacebookShareButton url={this.props.fileURL} />
+					<LinkedinShareButton url={this.props.fileURL} />
+					<PinterestShareButton url={this.props.fileURL} />
+					<PocketShareButton url={this.props.fileURL} />
+					<RedditShareButton url={this.props.fileURL} />
+					<TumblrShareButton url={this.props.fileURL} />
+					<TwitterShareButton url={this.props.fileURL} />
+					<WhatsappShareButton url={this.props.fileURL} />
+				</header>
+			</div>
+		);
+	}
 }
 
-export default Header;
+export default Share;
