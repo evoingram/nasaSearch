a6569223d0566e4645c100d4d90c75a7ff87d8ff
Erica L. Ingram <inquiries@aquoco.co>
2020-04-01 19:07:26 -0700

    working on adding testing, going to deploy for now

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { initialState, reducer } from './reducers/index.js';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// jest.mock('../api');
+
+function renderWithRedux(ui, { initialState, store = createStore(reducer, initialState) } = {}) {
+	return {
+		...render(<Provider store={store}>{ui}</Provider>),
+		// adding `store` to the returned utilities to allow us
+		// to reference it in our tests (just try to avoid using
+		// this to test implementation details).
+		store
+	};
+}
+
+/*
+test('can render with redux with defaults', () => {
+	const { getByTestId, getByText } = renderWithRedux(<App />);
+	const linkElement = getByText(/Welcome to Erica Ingram's NASA Multimedia Library Search React Redux Clone/i);
+	expect(linkElement).toBeInTheDocument();
+});
+*/
+test('renders welcome text', () => {
+	const { getByText } = render(<App />);
+	const linkElement = getByText(/Welcome to Erica Ingram's NASA Multimedia Library Search React Redux Clone/i);
+	expect(linkElement).toBeInTheDocument();
 });
